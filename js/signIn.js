5a9ab3b24d625af6649d40fbffaf7833846410be
Henry <htrinh@g.hmc.edu>
2021-08-03 23:40:42 +0000

    Fixed

--- js/signIn.js
+++ js/signIn.js
@@ -1,5 +1,4 @@
 //Have you visited the landing page before? Check using cookie
-document.cookie = "landingPage=visited; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 
 const signIn = () => {
   var provider = new firebase.auth.GoogleAuthProvider();
